9cbb38b4f05588d66826b7f0ff3f56b7c3c51ac6
iko <ilyakooo0@gmail.com>
2019-09-28 22:30:07 +0300

    Added some more arrows

--- src/Control/Arrow/Extra.hs
+++ src/Control/Arrow/Extra.hs
@@ -2,5 +2,8 @@ module Control.Arrow.Extra
     ( module X
     ) where
 
-import Control.Arrow.Extra.BaseArrow as X
-import Control.Arrow.Extra.PolyArrow as X
+import           Control.Arrow.Extra.ArrowChoice as X
+import           Control.Arrow.Extra.ArrowPlus   as X
+import           Control.Arrow.Extra.ArrowZero   as X
+import           Control.Arrow.Extra.BaseArrow   as X
+import           Control.Arrow.Extra.PolyArrow   as X
